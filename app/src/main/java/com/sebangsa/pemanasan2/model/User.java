7508cf3669bca1de5aebcb422c2403b45730d115
triwahyuprasetyo <triwahyuprasetyo@gmail.com>
2016-09-07 12:54:08 +0700

    update : User extends RealmObject, tambah RealmService untuk simpan dan retrieve users

--- app/src/main/java/com/sebangsa/pemanasan2/model/User.java
+++ app/src/main/java/com/sebangsa/pemanasan2/model/User.java
@@ -3,12 +3,13 @@ package com.sebangsa.pemanasan2.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 
 /**
  * Created by sebangsa on 8/30/16.
  */
-public class User {
+public class User  extends RealmObject {
 
     @PrimaryKey
     @Expose
---
+++ app/src/main/java/com/sebangsa/pemanasan2/service/RealmService.java
@@ -0,0 +1,48 @@
+package com.sebangsa.pemanasan2.service;
+
+import android.content.Context;
+
+import com.sebangsa.pemanasan2.model.User;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmResults;
+
+/**
+ * Created by sebangsa on 9/7/16.
+ */
+public class RealmService {
+    private static RealmService realmService;
+    private final String LOG_TAG = "REALM SERVICE";
+    private Realm realm;
+
+    public RealmService() {
+        realm = Realm.getDefaultInstance();
+    }
+
+    public static RealmService getRealmService(Context context) {
+        RealmConfiguration realmConfig = new RealmConfiguration
+                .Builder(context)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(realmConfig);
+        if (realmService == null) {
+            realmService = new RealmService();
+        }
+        return realmService;
+    }
+
+    public void saveUser(final User user) {
+
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realmm) {
+                realmm.copyToRealmOrUpdate(user);
+            }
+        });
+    }
+
+    public RealmResults<User> getUsers() {
+        return realm.where(User.class).findAll();
+    }
+}
