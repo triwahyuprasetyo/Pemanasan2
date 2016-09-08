60194e844a441c36aecd801815ecfbb84857c818
triwahyuprasetyo <triwahyuprasetyo@gmail.com>
2016-09-08 13:20:30 +0700

    update tampilan profile activity

--- app/src/main/java/com/sebangsa/pemanasan2/ProfileActivity.java
+++ app/src/main/java/com/sebangsa/pemanasan2/ProfileActivity.java
@@ -8,13 +8,26 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.sebangsa.pemanasan2.model.UserRealm;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textViewUsername;
+    private TextView textViewName;
+    private TextView textViewLocation;
+    private TextView textViewBio;
+    private TextView textViewPublicPost;
+    private TextView textViewCommunity;
+    private TextView textViewFollowing;
+    private TextView textViewFollowers;
+    private Button buttonMention;
+    private Button buttonFollowing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +36,29 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             }
         });
+        textViewUsername = (TextView) findViewById(R.id.textView_username);
+        textViewName = (TextView) findViewById(R.id.textView_name);
+        textViewLocation = (TextView) findViewById(R.id.textView_location);
+        textViewBio = (TextView) findViewById(R.id.textView_bio);
+        textViewPublicPost = (TextView) findViewById(R.id.textView_public_post);
+        textViewCommunity = (TextView) findViewById(R.id.textView_community);
+        textViewFollowing = (TextView) findViewById(R.id.textView_following);
+        textViewFollowers = (TextView) findViewById(R.id.textView_followers);
+        buttonFollowing = (Button) findViewById(R.id.button_following);
+        buttonFollowing.setOnClickListener(this);
+        buttonMention = (Button) findViewById(R.id.button_mention);
+        buttonMention.setOnClickListener(this);
+
         EventBus.getDefault().register(this);
+
+
     }
 
     @Override
@@ -39,11 +68,23 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     @Subscribe(sticky = true)
-    public void onUserRealmEvent(UserRealm user) {
-        if (user != null) {
-            updateView(user);
-            Log.i("UserRealm", user.getUsername() + " : " + user.getName());
-            EventBus.getDefault().removeStickyEvent(UserRealm.class);
+    public void onUserRealmEvent(UserRealm u) {
+        if (u != null) {
+            Log.i("UserRealm", u.getUsername() + " : " + u.getName());
+            Log.i("FOLLOWING", u.getId() + ", " + u.getUsername() + ", " + u.getBio() + ", "
+                    + u.getName() + ", " + u.isFollow() + ", " + u.getMedium() + ", " + u.getFollowing() + ", "
+                    + u.getFollowers());
+            textViewUsername.setText("@" + u.getUsername());
+            textViewName.setText(u.getName());
+            textViewLocation.setText("Loc -");
+            textViewBio.setText(u.getBio());
+            textViewPublicPost.setText("0");
+            textViewCommunity.setText("0");
+            textViewFollowing.setText(u.getFollowing()+"");
+            textViewFollowers.setText(u.getFollowers()+"");
+            Log.i("UserRealm", u.getUsername() + " : " + u.getName());
+
+            //EventBus.getDefault().removeStickyEvent(UserRealm.class);
         }
     }
 
@@ -54,7 +95,13 @@ public class ProfileActivity extends AppCompatActivity {
         return true;
     }
 
-    private void updateView(UserRealm user) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
