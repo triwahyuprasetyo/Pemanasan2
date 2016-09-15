fd1312adb1a9ea036aa56e0700172b9e947b5b56
triwahyuprasetyo <triwahyuprasetyo@gmail.com>
2016-09-15 10:32:04 +0700

    tambah cara retrieve data menggunakan rx di class RetrofitService

--- app/src/main/java/com/sebangsa/pemanasan2/service/RetrofitService.java
+++ app/src/main/java/com/sebangsa/pemanasan2/service/RetrofitService.java
@@ -16,14 +16,42 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observer;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by sebangsa on 8/31/16.
  */
 public class RetrofitService {
+    private static final String TAG = "RETROFIT_SERVICE";
     private static final String BASE_URL = "http://hangga.web.id/";
     private static RetrofitService retrofitService;
     private SebangsaService service;
+    private Subscription subscription;
+    private Observer<UserWrapper> sebangsaObserver = new Observer<UserWrapper>() {
+
+        @Override
+        public void onCompleted() {
+            subscription.unsubscribe();
+            Log.i(TAG, "Observer Complete & unSubscribe");
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Log.d(TAG, ">>>> onError gets called : " + e.getMessage());
+            Log.i(TAG, "Retrieve Following Users Error");
+            EventBus.getDefault().post(new MessageEvent("Failure"));
+        }
+
+        @Override
+        public void onNext(UserWrapper users) {
+            List<User> userList = users.getUsers();
+            Log.i(TAG, "Retrieve Following Users Success" + userList.size());
+            EventBus.getDefault().post(userList);
+        }
+    };
 
     public RetrofitService() {
         Retrofit retrofit = new Retrofit.Builder()
@@ -47,6 +75,14 @@ public class RetrofitService {
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
         return retrofit.create(SebangsaService.class);
+    }
+
+    public void retrieveAllFollowingUsers() {
+        final SebangsaService service = createRetrofitClient();
+        subscription = service.getAllFollowingUsers()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(sebangsaObserver);
     }
 
     public void retrieveFollowingUsers2() {
