fb42b022565b56d0ab36c85f4301d940bda97449
triwahyuprasetyo <triwahyuprasetyo@gmail.com>
2016-09-15 09:53:40 +0700

    tambah method dg ret Observable di SebangsaService

--- app/src/main/java/com/sebangsa/pemanasan2/service/SebangsaService.java
+++ app/src/main/java/com/sebangsa/pemanasan2/service/SebangsaService.java
@@ -4,6 +4,7 @@ import com.sebangsa.pemanasan2.model.UserWrapper;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import rx.Observable;
 
 /**
  * Created by sebangsa on 8/30/16.
@@ -11,4 +12,7 @@ import retrofit2.http.GET;
 public interface SebangsaService {
     @GET("sdp-latihan/following.php")
     Call<UserWrapper> getFollowingUsers2();
+
+    @GET("sdp-latihan/following.php")
+    Observable<UserWrapper> getAllFollowingUsers();
 }
