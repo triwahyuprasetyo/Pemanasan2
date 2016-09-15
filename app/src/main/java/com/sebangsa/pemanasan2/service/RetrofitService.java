2496c8190f8ed259e5a5777b1f64851d1a95894c
triwahyuprasetyo <triwahyuprasetyo@gmail.com>
2016-09-15 11:28:23 +0700

    class RetrofitService --> hapus code retrieve data cara retrofit tanpa rxAndroid

--- app/src/main/java/com/sebangsa/pemanasan2/service/RetrofitService.java
+++ app/src/main/java/com/sebangsa/pemanasan2/service/RetrofitService.java
@@ -10,9 +10,6 @@ import org.greenrobot.eventbus.EventBus;
 
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -30,35 +27,16 @@ public class RetrofitService {
     private static RetrofitService retrofitService;
     private SebangsaService service;
     private Subscription subscription;
-    private Observer<UserWrapper> sebangsaObserver = new Observer<UserWrapper>() {
-
-        @Override
-        public void onCompleted() {
-            subscription.unsubscribe();
-            Log.i(TAG, "Observer Complete & unSubscribe");
-        }
-
-        @Override
-        public void onError(Throwable e) {
-            Log.d(TAG, ">>>> onError gets called : " + e.getMessage());
-            Log.i(TAG, "Retrieve Following Users Error");
-            EventBus.getDefault().post(new MessageEvent("Failure"));
-        }
-
-        @Override
-        public void onNext(UserWrapper users) {
-            List<User> userList = users.getUsers();
-            Log.i(TAG, "Retrieve Following Users Success" + userList.size());
-            EventBus.getDefault().post(userList);
-        }
-    };
+    private Observer<UserWrapper> sebangsaObserver;
 
     public RetrofitService() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
         service = retrofit.create(SebangsaService.class);
+        initObserverSebangsa();
     }
 
     public static RetrofitService getRetrofitServiceInstance() {
@@ -68,38 +46,35 @@ public class RetrofitService {
         return retrofitService;
     }
 
-    public SebangsaService createRetrofitClient() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .build();
-        return retrofit.create(SebangsaService.class);
+    private void initObserverSebangsa() {
+        sebangsaObserver = new Observer<UserWrapper>() {
+
+            @Override
+            public void onCompleted() {
+                subscription.unsubscribe();
+                Log.i(TAG, "Observer Complete & unSubscribe");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, ">>>> onError gets called : " + e.getMessage());
+                Log.i(TAG, "Retrieve Following Users Error");
+                EventBus.getDefault().post(new MessageEvent("Failure"));
+            }
+
+            @Override
+            public void onNext(UserWrapper users) {
+                List<User> userList = users.getUsers();
+                Log.i(TAG, "Retrieve Following Users Success" + userList.size());
+                EventBus.getDefault().post(userList);
+            }
+        };
     }
 
     public void retrieveAllFollowingUsers() {
-        final SebangsaService service = createRetrofitClient();
         subscription = service.getAllFollowingUsers()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(sebangsaObserver);
-    }
-
-    public void retrieveFollowingUsers2() {
-        Call<UserWrapper> call = service.getFollowingUsers2();
-        call.enqueue(new Callback<UserWrapper>() {
-            @Override
-            public void onResponse(Call<UserWrapper> call, Response<UserWrapper> response) {
-                List<User> userList = response.body().getUsers();
-                Log.i("FOLLOWING", "Retrieve Following Users Success" + userList.size());
-                EventBus.getDefault().post(userList);
-            }
-
-            @Override
-            public void onFailure(Call<UserWrapper> call, Throwable t) {
-                Log.i("FOLLOWING", "Retrieve Following Users Error");
-                EventBus.getDefault().post(new MessageEvent("Failure"));
-            }
-        });
     }
 }
